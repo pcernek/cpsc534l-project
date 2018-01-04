fa37c922d68a85d615a2c8de870bd1d0fa6d8255
Paul Cernek <ptcernek@gmail.com>
2018-01-03 18:24:17 -0800

    Make it so that git doesn't track changes to the debug flag

--- code/src.cmake
+++ code/src.cmake
@@ -12,6 +12,11 @@ set(SOURCE_FILES
         src/network/node.h src/network/skill.h src/io/instance_loader.cpp src/io/instance_loader.h src/network/graph.cpp src/network/graph.h src/instance.h src/task/task_distribution.h src/solver/greed_ratio_top_k_solver.cpp src/solver/greed_ratio_top_k_solver.h src/typedefs.h src/solver/top_k_solver.h src/cost/cost_calculator.h src/utility/utility_calculator.h src/constants.h src/messages.h src/solver/bottom_k_solver.h src/cost/edge_weight_summer.cpp src/cost/edge_weight_summer.h src/task/independent_task_distribution.cpp src/task/independent_task_distribution.h src/utility/expected_coverage_calculator.cpp src/utility/expected_coverage_calculator.h src/task/constant_task_distribution.cpp src/task/constant_task_distribution.h src/solver/ratio_solver.h src/debug.h
         src/cost/team_inclusion_cost_adder.cpp src/cost/team_inclusion_cost_adder.h)
 
+# A bit of a hack to avoid git tracking changes to the file that flags debug mode:
+# gitignore it, and have the build system automatically generate one that can then be tampered with as desired
+# Do note that this currently gets reset every time you run cmake... :-(
+configure_file(src/debug.h.in ${SOURCE_DIR}/debug.h COPYONLY)
+
 # Create target for library of main functionality
 set(SRC_LIB ${PROBLEM_NAME})
 add_library(${SRC_LIB} STATIC ${SOURCE_FILES})
